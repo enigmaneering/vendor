7fb2da9d4058558110ce69afbfeaf4ce493eadf7
Robert Petz <rpetz@ignitelabs.net>
2026-03-15 01:09:20 -0700

    ...

--- scripts/build-glslang.sh
+++ scripts/build-glslang.sh
@@ -100,8 +100,8 @@ if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" || "$OSTYPE" == "cygwin" ]]; t
         CMAKE_CXX_COMPILER="-DCMAKE_CXX_COMPILER=/mingw64/bin/clang++.exe"
         CMAKE_LINKER="-DCMAKE_LINKER=/mingw64/bin/ld.lld.exe"
         CMAKE_SYSTEM_PROCESSOR="-DCMAKE_SYSTEM_PROCESSOR=aarch64"
-        CMAKE_C_FLAGS="-DCMAKE_C_FLAGS=--target=aarch64-w64-mingw32 --sysroot=/mingw64/aarch64-w64-mingw32"
-        CMAKE_CXX_FLAGS="-DCMAKE_CXX_FLAGS=--target=aarch64-w64-mingw32 --sysroot=/mingw64/aarch64-w64-mingw32"
+        CMAKE_C_FLAGS="-DCMAKE_C_FLAGS=--target=aarch64-w64-mingw32;--sysroot=/mingw64/aarch64-w64-mingw32"
+        CMAKE_CXX_FLAGS="-DCMAKE_CXX_FLAGS=--target=aarch64-w64-mingw32;--sysroot=/mingw64/aarch64-w64-mingw32"
         CMAKE_EXE_LINKER_FLAGS="-DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=lld;-L/mingw64/aarch64-w64-mingw32/lib"
     fi
 fi
